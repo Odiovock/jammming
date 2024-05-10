0ec0111fa7e65b24d5b89e607f6a71a01a6585a4
Odiovock <64711277+Odiovock@users.noreply.github.com>
2024-05-10 13:44:05 -0400

    Test data and title change with useEffect in app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar';
 
 function App() {
+  const [tracks, setTracks] = useState(
+    {
+      title: "Song of test",
+      artist: "Zelda",
+      album: "Ocarina fire"
+    },
+    {
+      title: "Song of time",
+      artist: "Zelda",
+      album: "Ocarina oldies"
+    }
+  );
+
+  useEffect(() => {
+    document.title = "Spotify App";
+  }, []);
+
   return (
     <div>
       <SearchBar/>
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function SearchResults () {
+function SearchResults (props)  {
 
 }
 
